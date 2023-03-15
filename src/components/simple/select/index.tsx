148bf90add94e260e9ca588f0fe4acd00c9ab2d3
Matthew Blanco <mblanco@recurforever.com>
2023-03-15 14:53:00 -0400

    Add Select component

---
+++ src/components/simple/select/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Select(props: 
+    {
+        children: JSX.Element
+        value: string
+        className?: string
+        defaultValue?: string
+        id?: string
+        isDisabled?: boolean
+        isRequired?: boolean
+        name?: string
+        onBlur?: React.FocusEventHandler<HTMLSelectElement>
+        onChange?: React.ChangeEventHandler<HTMLSelectElement>
+        onFocus?: React.FocusEventHandler<HTMLSelectElement>
+        placeholder?: string
+    }) {   
+    return (
+        <select 
+            className={props.className}
+            defaultValue={props.defaultValue}
+            disabled={props.isDisabled}
+            id={props.id}
+            name={props.name}
+            onBlur={props.onBlur}
+            onChange={props.onChange}
+            onFocus={props.onFocus}
+            placeholder={props.placeholder}
+            required={props.isRequired}
+            value={props.value}
+        >
+            { props.children }
+        </select>
+    )
+}
