fc6578bd130a770e3c80c36700d399ae1d7e742e
Matthew Blanco <mblanco@recurforever.com>
2023-03-15 15:25:19 -0400

    Add Form component

---
+++ src/components/intermediate/form/index.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Form(props:
+    {
+        children: JSX.Element
+        className?: string
+        id?: string
+        name?: string
+        disabled?: boolean
+        form?: string
+    }) {
+    return (
+        <form
+        id={ props.id }
+        className={ props.className }
+        >
+            <fieldset
+                name={ props.name }
+                disabled={ props.disabled }
+                form={ props.form }
+            >
+                { props.children }
+            </fieldset>
+        </form>
+    )
+}
