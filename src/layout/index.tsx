a7ae63551043bb6f4949bfbedcbac2a213cfd477
Matthew Blanco <mblanco@recurforever.com>
2023-03-26 14:19:46 -0400

    Export layout components

---
+++ src/layout/index.tsx
@@ -0,0 +1,5 @@
+// Layout components:
+export { default as LayoutFooter } from "./layout-footer"
+export { default as LayoutHeader } from "./layout-header"
+export { default as QueryMenu } from "./query-menu"
+export { default as VisualizationViewport } from "./visualization-viewport"
