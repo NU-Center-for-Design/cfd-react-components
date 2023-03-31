db55b6b04ee96b5621c3e534a12aa458d800ff46
Matthew Blanco <mblanco@recurforever.com>
2023-03-31 15:25:55 -0400

    Remove unused imports

--- src/components/intermediate/accordion/index.tsx
+++ src/components/intermediate/accordion/index.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-const CloseArrow = require('./assets/close-arrow.svg')
-const OpenArrow = require('./assets/open-arrow.svg')
 
 export default function Accordion(props:
     {
