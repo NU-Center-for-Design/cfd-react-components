1239612231efae44c55c07f7cd7b351f11b7ee75
Matthew Blanco <mblanco@recurforever.com>
2023-03-31 16:59:14 -0400

    FIX: Hide Accordion useState

--- src/components/intermediate/accordion/index.tsx
+++ src/components/intermediate/accordion/index.tsx
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React from 'react';
 
 export default function Accordion(props:
     {
         children: JSX.Element
+        onClick?: (e?: React.MouseEvent<HTMLElement>) => void
         className?: string
         id?: string
+        openSrc?: string
+        closeSrc?: string
     }) {
+        // const [isOpen, setOpen] = React.useState(false)
+        // // console.log("test", isOpen)
+        // const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        //     // setOpen(!isOpen)
+        //     if (props.onClick) {
+        //         props.onClick(event)
+        //     }
+        // }
+
     return (
         <details 
-            style={{display: 'flex'}}
             className={props.className}
             id={ props.id }
         >
             { props.children }
+            {
+                    (props.openSrc && props.closeSrc) && (
+                        <img src="" /> //{isOpen ? props.openSrc : props.closeSrc}/>
+                    ) 
+            } 
         </details>
     )
   }
--- src/components/simple/accordion-header/index.tsx
+++ src/components/simple/accordion-header/index.tsx
@@ -4,26 +4,14 @@ export default function AccordionHeader(props:
     {
         children: JSX.Element
         onClick?: (e?: MouseEvent<HTMLElement>) => void
-        openSrc?: string
-        closeSrc?: string
     }) {
-        const [isOpen, setOpen] = useState(false)
-
-        const handleClick = (event: MouseEvent<HTMLElement>) => {
-            setOpen(!isOpen)
-            if (props.onClick) {
-                props.onClick(event)
-            }
-        }
-
     return (
-        <summary className={'accordion-heading'} onClick={handleClick}>
-            { props.children }
-            {
-                    (props.openSrc && props.closeSrc) && (
-                        <img src={isOpen ? props.openSrc : props.closeSrc}/>
-                    ) 
-            }      
+        <summary 
+        style={{display: 'contents'}}
+        className={'accordion-heading'}
+         onClick={props.onClick}
+        >
+            { props.children }     
         </summary>
     )
 }
