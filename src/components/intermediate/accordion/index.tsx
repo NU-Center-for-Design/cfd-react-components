4de2a773bec8bd77f26f478080f6c7c05a7f40f9
Matthew Blanco <mblanco@recurforever.com>
2023-03-31 15:22:38 -0400

    Allow for unique Accordion assets sources

--- src/components/intermediate/accordion/index.tsx
+++ src/components/intermediate/accordion/index.tsx
@@ -7,14 +7,27 @@ export default function Accordion(props:
         children: JSX.Element
         className?: string
         id?: string
+        openSrc?: string
+        closeSrc?: string
     }) {
         const [isOpen, setOpen] = useState(false)
 
+        const handleClick = () => {
+            setOpen(!isOpen)
+        }
+
     return (
-        <details style={{display: 'flex'}} className={props.className} id={ props.id }>
+        <details 
+            style={{display: 'flex'}}
+            className={props.className}
+            id={ props.id }
+            onClick={handleClick}
+        >
             { props.children }
             {
-                isOpen ? <OpenArrow /> : <CloseArrow />
+                (props.openSrc && props.closeSrc) && (
+                    <img src={isOpen ? props.openSrc : props.closeSrc}/>
+                ) 
             }
         </details>
     )
