2c70e78b5190421f0d194f8532767eda56ff9dcd
Matthew Blanco <mblanco@recurforever.com>
2023-04-16 21:50:24 -0400

    Add required properties to checkbox

--- src/components/intermediate/checkbox/index.tsx
+++ src/components/intermediate/checkbox/index.tsx
@@ -3,15 +3,15 @@ import Label from '../../simple/label'
 
 export default function Checkbox(props: 
     {
-        className?: string
-        id?: string
-        labelClassName?: string
-        label: JSX.Element
-        checked?: boolean
-        disabled?: boolean
-        onChange?: React.ChangeEventHandler<HTMLInputElement>
-        name?: string
-        value?: string | number
+      id: string
+      name: string
+      className?: string
+      labelClassName?: string
+      label: JSX.Element
+      checked?: boolean
+      disabled?: boolean
+      onChange?: React.ChangeEventHandler<HTMLInputElement>
+      value?: string | number
     }) {
     return (
       <div>
@@ -23,9 +23,11 @@ export default function Checkbox(props:
             onChange={ props.onChange }
             checked={ props.checked }
             className={ props.className }
+            id={ props.id }
           />
         <Label
           className={ props.labelClassName }
+          for={ props.id }
         >
           { props.label }
         </Label>
